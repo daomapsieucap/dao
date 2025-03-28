1d50645b1b4dc9c32e7c880545082a9336ea6f40
daomapsieucap <bichdao.chau@gmail.com>
2025-03-28 21:21:26 +0700

    feat(buid): update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt({
+    sitemap: `${SITE_URL}/sitemap.xml`,
+  }),],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
