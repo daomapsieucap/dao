efe5183f9e396ce0e21f28c82320282036785140
daomapsieucap <bichdao.chau@gmail.com>
2025-05-24 20:13:18 +0700

    feat(ga): add ga script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,31 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
+import {SITE_URL} from "./src/data/config";
+import partytown from '@astrojs/partytown'
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt({
-    sitemap: `${SITE_URL}/sitemap.xml`,
-  }),],
-  site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false
-    }
-  }
+    integrations: [
+        tailwind(),
+        sitemap(),
+        robotsTxt({
+            sitemap: `${SITE_URL}/sitemap.xml`,
+        }),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+    site: SITE_URL,
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            theme: "nord",
+            wrap: false
+        }
+    },
 });
