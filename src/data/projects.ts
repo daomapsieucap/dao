3ff9a8ee1d664969f710b9764d1559b9764a1c66
daomapsieucap <bichdao.chau@gmail.com>
2026-06-12 11:06:57 +0700

    feat(projects): update list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,68 +1,82 @@
 export type Project = {
-  title: string;
-  platform: string;
-  role: string;
-  type: string;
-  link: string;
+    title: string;
+    platform: string;
+    role: string;
+    type: string;
+    link: string;
 };
 
 const projects: Project[] = [
-  {
-    title: "SleepyPants",
-    platform: "shopify",
-    role: "backend",
-    type: "theme customization",
-    link: "https://sleepypants.co.uk/",
-  },
-  {
-    title: "Là Nhà Academy",
-    platform: "wordpress",
-    role: "backend",
-    type: "built from scratch",
-    link: "https://lanhaacademy.vn/",
-  },
-  {
-    title: "Southern Star",
-    platform: "shopify",
-    role: "fullstack",
-    type: "theme customization",
-    link: "https://southernstarus.com/",
-  },
-  {
-    title: "B-RISE",
-    platform: "wordpress",
-    role: "backend",
-    type: "built from scratch",
-    link: "https://b-rise.asia/",
-  },
-  {
-    title: "AnyAlterations",
-    platform: "wordpress",
-    role: "fullstack",
-    type: "theme customization",
-    link: "https://anyalterations.com/",
-  },
-  {
-    title: "Là Nhà",
-    platform: "wordpress",
-    role: "backend",
-    type: "theme customization",
-    link: "https://www.lanha.vn/",
-  },
-  {
-    title: "Vizulize",
-    platform: "wordpress",
-    role: "fullstack",
-    type: "theme customization",
-    link: "https://vizulize.vn/",
-  },
-  {
-    title: "SNKRS Nation",
-    platform: "html",
-    role: "fullstack",
-    type: "just for fun",
-    link: "https://snkrsnation.daochau.com/",
-  },
+    {
+        title: "AnyAlterations",
+        platform: "wordpress",
+        role: "fullstack",
+        type: "theme customization",
+        link: "https://anyalterations.com/",
+    },
+    {
+        title: "SleepyPants",
+        platform: "shopify",
+        role: "fullstack",
+        type: "theme customization",
+        link: "https://sleepypants.co.uk/",
+    },
+    {
+        title: "SuperPart",
+        platform: "wordpress",
+        role: "fullstack",
+        type: "theme customization",
+        link: "https://superpart.vn/",
+    },
+    {
+        title: "Là Nhà Academy",
+        platform: "wordpress",
+        role: "backend",
+        type: "built from scratch",
+        link: "https://lanhaacademy.vn/",
+    },
+    {
+        title: "Lọc Nước Xanh",
+        platform: "wordpress",
+        role: "backend",
+        type: "built from scratch",
+        link: "https://locnuocxanh247.vn/",
+    },
+    {
+        title: "Ẩm thực sinh thái Xuân Huơng",
+        platform: "wordpress",
+        role: "backend",
+        type: "built from scratch",
+        link: "https://sinhthaixuanhuong.com.vn/",
+    },
+    {
+        title: "Southern Star",
+        platform: "shopify",
+        role: "fullstack",
+        type: "theme customization",
+        link: "https://southernstarus.com/",
+    },
+    {
+        title: "B-RISE",
+        platform: "wordpress",
+        role: "backend",
+        type: "built from scratch",
+        link: "https://b-rise.asia/",
+    },
+    {
+        title: "Vizulize",
+        platform: "wordpress",
+        role: "fullstack",
+        type: "theme customization",
+        link: "https://vizulize.vn/",
+    },
+    {
+        title: "SNKRS Nation",
+        platform: "html",
+        role: "fullstack",
+        type: "just for fun",
+        link: "https://snkrsnation.daochau.com/",
+    },
 ];
 
 export default projects;
